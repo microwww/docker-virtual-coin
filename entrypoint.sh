c2c93d2c61bc4bf4dffdb3bb28991620e25bd0af
changshu.li <lcs.005@163.com>
2019-08-07 19:31:48 +0800

    gener

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,33 +1,38 @@
 #!/bin/sh
+set -e
 
-echo ${PASSWORD:=123456} >/root/eth.pass
-
+pass='/root/.ethereum/eth.pass'
 gen='/root/.ethereum/genesis.json'
-if [ ! -f "${gen}" ];then
 
+mkdir -p /root/.ethereum/
+echo ${PASSWORD:=123456} > ${pass}
+
+if [ ! -f "${gen}" ] ; then
+
+	/usr/local/bin/geth account new --password ${pass} > /dev/null 2>&1
+
+	address=`geth account list 2>/dev/null | sed -n '1p' | cut -d { -f2 |cut -d } -f1`
     if [ ! "${#NONCE}" == 18 ] ; then
-        address=`/usr/local/bin/geth account new --password /root/eth.pass 2>/dev/null |grep '0x'`
         NONCE=${address##* }
-        NONCE=${NONCE:0:18}
-    #    NONCE=`echo ${NONCE} | tr '[A-Z]' '[a-z]'`
+        NONCE=${NONCE:0:15}
+        #NONCE=`echo ${NONCE} | tr '[A-Z]' '[a-z]'`
     fi
 
     echo '{
       "config": {
-          "chainId": 6,
+          "chainId": '`echo "0x0"${address:0:6} | xargs printf %o`',
           "homesteadBlock": 0,
           "eip155Block": 0,
           "eip158Block": 0
       },
       "alloc": {
-          "0x0000000000000000000000000000000000000001": {"balance": "9876543210"},
-          "0x0000000000000000000000000000000000000002": {"balance": "12345678901"}
+          "0x0000000000000000000000000000000000000001": {"balance": "1"}
       },
-      "coinbase"   : "0x0000000000000000000000000000000000000000",
+      "coinbase"   : "0x'${address}'",
       "difficulty" : "0x20000",
       "extraData"  : "",
       "gasLimit"   : "0x2fefd8",
-      "nonce"      : "'${NONCE}'",
+      "nonce"      : "0x0'${NONCE}'",
       "mixhash"    : "0x0000000000000000000000000000000000000000000000000000000000000000",
       "parentHash" : "0x0000000000000000000000000000000000000000000000000000000000000000",
       "timestamp"  : "0x00"
@@ -36,8 +41,9 @@ if [ ! -f "${gen}" ];then
 fi
 
 if [ ! -d "/root/.ethereum/geth" ];then
-    geth init ${gen}
+    geth init ${gen} 2>/dev/null
 fi
 
 /usr/local/bin/geth $@
 ## /bin/sh
+
